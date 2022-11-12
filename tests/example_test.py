c4679860e4b316af674d5af83d5f0dd6ee506b2c
Daniel Ebert <iuq@web.de>
2022-11-12 13:18:14 -0800

    put all src files in separate folder

--- tests/example_test.py
+++ tests/example_test.py
@@ -1,8 +1,8 @@
-from example import add
-from example import mul
+from templates.example import add
+from templates.example import mul
 
 def test_add() -> None:
-  assert(add(1, 3) == 4)
+  assert add(1, 3) == 4
 
 def test_mul() -> None:
-  assert(mul(2, 3) == 6)
+  assert mul(2, 3) == 6
